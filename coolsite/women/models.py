c7c9e43fce8b04c9893eca33c0a37f2102e05089
Ivan Verbenko <trimurs@gmail.com>
2022-11-25 16:25:57 +0300

    Добавил url адреса

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Women(models.Model):
     title=models.CharField(max_length=255)
     content=models.TextField(blank=True)
@@ -8,3 +11,9 @@ class Women(models.Model):
     time_create=models.DateTimeField(auto_now_add=True)
     time_update=models.DateTimeField(auto_now=True)
     is_published=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 # Create your views here.
-menu=['о сайте', 'добавить статью','os', 'login']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 def index(request):
     posts=Women.objects.all()
-    return render(request, 'women/index.html',{'title':'title','menu':menu,'posts':posts})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request,'women/about.html', {'title':'about','menu':menu})
@@ -16,3 +26,14 @@ def categories(request, catid):
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound('Стр не найдена')
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение стр {post_id}")
